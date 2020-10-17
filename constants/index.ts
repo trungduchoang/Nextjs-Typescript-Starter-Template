c4394b477a1ec49888747c6088c6b4d99f9e8a18
t-hoang-cna <t-hoang-cna@usen-next.jp>
2020-10-17 09:16:00 +0700

    Add src/

--- constants/index.ts
+++
@@ -1,9 +0,0 @@
-import PAGE_NAMES from "./pageNames";
-import ROUTES from "./routes";
-
-const CONSTANTS = {
-  PAGE_NAMES,
-  ROUTES,
-};
-
-export default CONSTANTS;
---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+import PAGE_NAMES from "./pageNames";
+import ROUTES from "./routes";
+
+const CONSTANTS = {
+  PAGE_NAMES,
+  ROUTES,
+};
+
+export default CONSTANTS;
