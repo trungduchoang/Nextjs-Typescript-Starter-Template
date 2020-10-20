bd69f1dec0f9a8120f3f20bc49844828ea571eac
t-hoang-cna <t-hoang@usen-next.jp>
2020-10-20 18:03:30 +0700

    CleanCode

--- src/mocks/Home/news.ts
+++ src/mocks/Home/news.ts
@@ -1,18 +1,18 @@
 export const news = [
   {
-    title: "Cung cấp lập trình viên",
+    title: "CITYNOW mừng Trung thu 2020",
     time: "2020/09/23 13:19:21",
     imgSrc:
       "http://citynow.vn/citynow/images/page/news/f29ba473648f9bd1c29e2.jpg",
   },
   {
-    title: "Phát triển phần mềm",
+    title: "CITYNOW và Ngày hội việc làm 2020",
     time: "2020/09/23 13:19:21",
     imgSrc:
       "http://citynow.vn/citynow/images/page/news/0c37611b0f34f36aaa25.jpg",
   },
   {
-    title: "Du học và việc làm",
+    title: "TALENT FRESHER 2020 CHÍNH THỨC KHỞI ĐỘNG",
     time: "2020/09/23 13:19:21",
     imgSrc:
       "http://citynow.vn/citynow/images/page/news/768cc5b063d69f88c6c7.jpg",
