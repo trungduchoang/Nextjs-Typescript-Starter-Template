43ec57c979e27cd3f514579b2e532f8630f3a379
t-hoang-cna <t-hoang@usen-next.jp>
2020-10-21 09:07:45 +0700

    CleanCode

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,17 +10,13 @@ import { cityNowSlide } from "@/mocks/Home/cityNowSlide";
 // others
 import style from "./Home.module.scss";
 
-const Home = () => {
-  // eslint-disable-next-line no-console
-  console.log("Dont forget Reset head");
-  return (
-    <div className={style.wrapper}>
-      <Slider slides={cityNowSlide} />
-      <CapacityInfo />
-      <Mission />
-      <News />
-    </div>
-  );
-};
+const Home = () => (
+  <div className={style.wrapper}>
+    <Slider slides={cityNowSlide} />
+    <CapacityInfo />
+    <Mission />
+    <News />
+  </div>
+);
 
 export default Home;
